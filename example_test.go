0cef9f3e023dd6354805d267983b9bfe78700ee6
Tim Schaub <tim.schaub@gmail.com>
2019-11-08 15:52:48 -0700

    Validation example

--- example_test.go
+++ example_test.go
@@ -26,7 +26,7 @@ func Example_basic() {
 }
 
 func Example_nested() {
-	str := `["or", ["gt", 10], ["lt", 20]]`
+	str := `["or", [">", 10], ["<", 20]]`
 	expression := &jiffy.Expression{}
 
 	err := json.Unmarshal([]byte(str), expression)
@@ -52,6 +52,55 @@ func Example_nested() {
 	// Output:
 	// operator: or
 	// number of arguments: 2
-	// first: gt [10]
-	// second: lt [20]
+	// first: > [10]
+	// second: < [20]
+}
+
+func Example_validation() {
+	// a custom validator for validating a "+" operator
+	expression := &jiffy.Expression{
+		Validator: func(operator string, arguments []interface{}) error {
+			switch operator {
+			case "+":
+				if len(arguments) != 2 {
+					return fmt.Errorf("the + operator takes two arguments, got %d", len(arguments))
+				}
+				for i, v := range arguments {
+					if _, ok := v.(float64); !ok {
+						return fmt.Errorf("expected number for argument %d, got %#v", i, v)
+					}
+				}
+				return nil
+			default:
+				return fmt.Errorf(`unsupported operator "%s"`, operator)
+			}
+		},
+	}
+
+	valid := `["+", 10, 32]`
+	if err := json.Unmarshal([]byte(valid), expression); err != nil {
+		log.Fatal("unexpected error:", err)
+	}
+	fmt.Printf("%s passes validation\n", valid)
+
+	notEnoughArgs := `["+", 42]`
+	if err := json.Unmarshal([]byte(notEnoughArgs), expression); err != nil {
+		fmt.Printf("%s fails validation: %s\n", notEnoughArgs, err)
+	}
+
+	wrongArgType := `["+", 10, "oops"]`
+	if err := json.Unmarshal([]byte(wrongArgType), expression); err != nil {
+		fmt.Printf("%s fails validation: %s\n", wrongArgType, err)
+	}
+
+	unsupportedOperation := `["oops", 42]`
+	if err := json.Unmarshal([]byte(unsupportedOperation), expression); err != nil {
+		fmt.Printf("%s fails validation: %s\n", unsupportedOperation, err)
+	}
+
+	// Output:
+	// ["+", 10, 32] passes validation
+	// ["+", 42] fails validation: the + operator takes two arguments, got 1
+	// ["+", 10, "oops"] fails validation: expected number for argument 1, got "oops"
+	// ["oops", 42] fails validation: unsupported operator "oops"
 }
